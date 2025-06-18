ee51f2a0d078b8fcec55d0b8c0bee4352d30cf91
Shinki.Nyx <bobby892007@gmail.com>
2025-06-18 16:34:10 +0700

    Update deleteHeader.js

--- shad-main/shad-main/js/deleteHeader.js
+++ shad-main/shad-main/js/deleteHeader.js
@@ -5,4 +5,4 @@
 const version = 'V1.0.2';
 
 
-function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag",""),$done({headers:modifiedHeaders});
+function setHeaderValue(e,a,d){var r=a.toLowerCase();r in e?e[r]=d:e[a]=d}var modifiedHeaders=$request.headers;setHeaderValue(modifiedHeaders,"X-RevenueCat-ETag","");setHeaderValue(modifiedHeaders,"x-logged-out","true");$done({headers:modifiedHeaders});
